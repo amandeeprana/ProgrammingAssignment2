0e951120632197ba1d4dc25d23f77f9674f7a185
Amandeep Singh Rana <Amandeep.Rana@Outlook.com>
2018-06-24 00:42:12 +0530

    Upload attempt 1 for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Defining a function makeCacheMatrix on an input matrix that will save that matrix as a special object
+## This will be understood by  second function (cacheSolve) can understand
+## Also will be Attaching --> 'set', 'get', 'setinverse', and 'getinverse' functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL ## Reset the value of i to NULL.. Clean sheet :)
+  set <- function(y) {
+    x <<- y
+    i <<- NULL ## i becomes NULL on run of set() even if it is 
+  }                   ## defined elsewhere (i.e. in another matrix)
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## If inverse of matrix x is calculated then is is returned from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("showing cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
+
+## TESTING Code
+## my_matrix<-makeCacheMatrix(matrix(1:4,2,2))
+## my_matrix$get()
+## cacheSolve(my_matrix)
+
+#> my_matrix<-makeCacheMatrix(matrix(1:4,2,2))
+#> my_matrix$get()
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+#> cacheSolve(my_matrix)
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+#> cacheSolve(my_matrix)
+#getting cached data
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
